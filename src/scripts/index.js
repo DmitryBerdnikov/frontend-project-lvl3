e2e877eb07f4a1999c21016b77686ffc7b95883b
DmitryBerdnikov <>
2021-03-22 22:02:51 +0300

    started validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,38 @@
+import * as yup from 'yup';
+
 import '../styles/main.scss';
+
+yup.setLocale({
+  string: {
+    url: 'Ссылка должна быть валидным URL',
+  },
+});
+
+// Посмотреть можно ли сделать test(match)
+// и также добавить на проверку пустой строки и написать текст ошибки пустой строки
+
+const schema = yup.string().url();
+
+document.querySelector('.js-form-rss').addEventListener('submit', (e) => {
+  const { target } = e;
+  const formData = new FormData(target);
+
+  const inputUrl = target.elements.url;
+
+  e.preventDefault();
+
+  const url = formData.get('url');
+
+  try {
+    schema.validateSync(url);
+  } catch (error) {
+    const { parentNode } = inputUrl;
+
+    parentNode.querySelector(
+      '.invalid-feedback',
+    ).textContent = error.errors.join();
+    inputUrl.classList.add('is-invalid');
+  }
+
+  e.preventDefault();
+});
