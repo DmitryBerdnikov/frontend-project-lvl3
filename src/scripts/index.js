21d70dc8c3ecebeab90c9e2a797f7670bb25161c
DmitryBerdnikov <>
2021-03-25 10:38:04 +0300

    added validation url

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,27 @@
 import * as yup from 'yup';
+import axios from 'axios';
 
 import '../styles/main.scss';
+
+const request = (url) => {
+  const URL_FOR_CROSS_ORIGINS_REQUESTS = 'https://hexlet-allorigins.herokuapp.com/get';
+
+  axios.get(`${URL_FOR_CROSS_ORIGINS_REQUESTS}?url=${encodeURIComponent(url)}`)
+    .then((response) => {
+      const { data } = response;
+
+      const parser = new DOMParser();
+
+      const xmlString = data.contents;
+      const doc1 = parser.parseFromString(xmlString, 'application/xml');
+
+      console.log(doc1.querySelector('title'));
+
+      console.log(doc1);
+    });
+};
+
+request('https://ru.hexlet.io/lessons.rss');
 
 yup.setLocale({
   string: {
@@ -8,10 +29,16 @@ yup.setLocale({
   },
 });
 
-// Посмотреть можно ли сделать test(match)
-// и также добавить на проверку пустой строки и написать текст ошибки пустой строки
-
-const schema = yup.string().url();
+const schema = yup.object().shape({
+  url: yup
+    .string()
+    .test(
+      'empty-check',
+      'Нужно заполнить поле',
+      (value) => value.length !== 0,
+    )
+    .url(),
+});
 
 document.querySelector('.js-form-rss').addEventListener('submit', (e) => {
   const { target } = e;
@@ -23,11 +50,16 @@ document.querySelector('.js-form-rss').addEventListener('submit', (e) => {
 
   const url = formData.get('url');
 
-  try {
-    schema.validateSync(url);
-  } catch (error) {
-    const { parentNode } = inputUrl;
+  const { parentNode } = inputUrl;
 
+  try {
+    schema.validateSync({ url });
+
+    parentNode.querySelector(
+      '.invalid-feedback',
+    ).textContent = '';
+    inputUrl.classList.remove('is-invalid');
+  } catch (error) {
     parentNode.querySelector(
       '.invalid-feedback',
     ).textContent = error.errors.join();
