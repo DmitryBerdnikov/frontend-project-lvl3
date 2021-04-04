4c4475d25b45b87f50be95574243a982ab47a71a
DmitryBerdnikov <>
2021-04-04 12:05:38 +0300

    adeded disabledCache

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,6 +9,7 @@ const send = (url) => {
 
   const generatedUrl = new URL(URL_FOR_CROSS_ORIGINS_REQUESTS);
   generatedUrl.searchParams.set('url', url);
+  generatedUrl.searchParams.set('disableCache', true);
 
   return axios.get(generatedUrl)
     .then((response) => {
