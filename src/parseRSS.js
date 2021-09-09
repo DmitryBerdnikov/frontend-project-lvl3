73d7d265c5277838f878372ac57d424c31ec332d
Berdnikov Dima <dmitry.berdnikov@fuse8.online>
2021-09-09 21:30:40 +0300

    refactor parseRSS

--- src/parseRSS.js
+++ src/parseRSS.js
@@ -1,45 +1,41 @@
-const createError = (name, message) => {
-  const error = new Error(message);
-
-  error.isParsingError = true;
-  error.name = name;
-
-  return error;
-};
-
 export default (xmlString) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(xmlString, 'application/xml');
-  const errorEl = doc.querySelector('parsererror');
+  try {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(xmlString, 'application/xml');
 
-  if (errorEl) {
-    throw createError('ParsingError', 'Parsing error');
+    const result = {
+      title: '',
+      description: '',
+      posts: [],
+    };
+
+    const titleEl = doc.querySelector('title');
+    const descriptionEl = doc.querySelector('description');
+
+    result.title = titleEl.textContent;
+    result.description = descriptionEl.textContent;
+
+    doc.querySelectorAll('item').forEach((element) => {
+      const postTitleEl = element.querySelector('title');
+      const postLinkEl = element.querySelector('link');
+      const postDescriptionEl = element.querySelector('description');
+
+      const newItem = {
+        title: postTitleEl.textContent,
+        link: postLinkEl.textContent,
+        description: postDescriptionEl.textContent,
+      };
+
+      result.posts = [...result.posts, newItem];
+    });
+
+    return result;
+  } catch (e) {
+    const error = new Error(e.message);
+
+    error.isParsingError = true;
+    error.name = 'ParsingError';
+
+    throw error;
   }
-
-  const result = {
-    title: '',
-    description: '',
-    posts: [],
-  };
-
-  const titleEl = doc.querySelector('title');
-  const descriptionEl = doc.querySelector('description');
-
-  result.title = titleEl.textContent;
-  result.description = descriptionEl.textContent;
-
-  doc.querySelectorAll('item').forEach((element) => {
-    const newItem = {};
-    const postTitleEl = element.querySelector('title');
-    const postLinkEl = element.querySelector('link');
-    const postDescriptionEl = element.querySelector('description');
-
-    newItem.title = postTitleEl.textContent;
-    newItem.link = postLinkEl.textContent;
-    newItem.description = postDescriptionEl.textContent;
-
-    result.posts = [...result.posts, newItem];
-  });
-
-  return result;
 };
